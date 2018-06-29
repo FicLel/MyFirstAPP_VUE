0b503024ab1078042b27842ec9e318ede4f69264
FicLel <victorleal190@gmail.com>
2018-06-28 21:12:49 -0600

    Conexion con API REST

--- webapp/src/RestaurantesList.vue
+++ webapp/src/RestaurantesList.vue
@@ -1,15 +1,49 @@
 <template>
-	<h2>Esto es la {{texto}}</h2>	
+	<div>
+		<ul id="restaurantes-list" v-if="restaurantes != null">
+			<li v-for="restaurante in restaurantes ">
+				<strong>{{restaurante.nombre}}</strong>
+			</li>
+		</ul>
+		<span v-else>Cargando restaurantes</span>
+	</div>	
 </template>
 
-
 <script>
+import axios from 'axios';
 export default {
   name: 'restaurantes-list',
+  mounted(){
+  	this.getRestaurantes();
+  },
   data () {
     return {
-      texto: 'Pagina de restaurantes'
+      texto: 'Pagina de restaurantes',
+      restaurantes: null
     }
-  }
+  },
+  methods: {
+  	getRestaurantes()
+  	{
+  		axios.get('http://localhost:8080/api-rest/restaurantes-api.php/restaurantes')
+  		 .then(respuesta => {this.restaurantes = respuesta.data.data;});
+
+  	}
+   }
 }
 </script>
+<style lang="scss">
+	#restaurantes-list{
+		padding: 5px;
+		li{
+			margin-top: 10px;
+			width: 30%;
+			height: 120px;
+			border: 1px solid #ddd;
+			background: #eee;
+			padding:20px;
+			overflow: hidden;
+		}
+	}
+
+</style>
